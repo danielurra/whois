bc0ec5df9076bc2c270ab7ac46138a4caace4807
Daniel Urra <danielurra75@gmail.com>
2025-08-15 18:43:26 -0500

    fix: centered suggested by chatgpt

--- public/javascript.js
+++ public/javascript.js
@@ -23,22 +23,62 @@ function runLookup(wan) {
   })
     .then((response) => response.json())
     .then((data) => {
-      const ispName = data.output.trim().split("\n").pop(); // Last line is usually ISP
       const logo = data.logo || "generic_logo.png";
 
       const formattedOutput = `
-        <div style="text-align:center">
-          <b>IP Address: ${ipInput}</b><br><br>
-          ${data.output.replace(/\n/g, "<br>")}<br><br>
-          <img src="/img/us_isp_logos/${logo}" alt="ISP Logo" style="max-height:80px; margin-top:10px;" />
+        <div class="result-container">
+          <div class="result-text">
+            <b>IP Address: ${ipInput}</b><br><br>
+            ${data.output.replace(/\n/g, "<br>")}
+          </div>
+          <div class="result-logo">
+            <img src="/img/us_isp_logos/${logo}" alt="ISP Logo" />
+          </div>
         </div>
       `;
+
       resultArea.innerHTML = formattedOutput;
     })
     .catch((error) => {
       resultArea.innerHTML = "<div style='text-align:center'><b>Error:</b> " + error + "</div>";
     });
 }
+
+// function runLookup(wan) {
+//   const ipInput = document.getElementById(`ipAddress-${wan}`).value.trim();
+//   const resultArea = document.getElementById(`result-${wan}`);
+//   const copyMessage = document.getElementById(`copyMessage-${wan}`);
+
+//   copyMessage.style.display = "none"; // Hide previous copy message
+
+//   if (!validateIP(ipInput)) {
+//     resultArea.innerHTML = "<div style='text-align:center'><b>Invalid IP Address!</b></div>";
+//     return;
+//   }
+
+//   fetch("/whois", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ ip: ipInput }),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const ispName = data.output.trim().split("\n").pop(); // Last line is usually ISP
+//       const logo = data.logo || "generic_logo.png";
+
+//       const formattedOutput = `
+//         <div style="text-align:center">
+//           <b>IP Address: ${ipInput}</b><br><br>
+//           ${data.output.replace(/\n/g, "<br>")}<br><br>
+//           <img src="/img/us_isp_logos/${logo}" alt="ISP Logo" style="max-height:80px; margin-top:10px;" />
+//         </div>
+//       `;
+//       resultArea.innerHTML = formattedOutput;
+//     })
+//     .catch((error) => {
+//       resultArea.innerHTML = "<div style='text-align:center'><b>Error:</b> " + error + "</div>";
+//     });
+// }
 
 function clearForm(wan) {
   document.getElementById(`ipAddress-${wan}`).value = "";
